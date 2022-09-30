7e44cb52279c00f8c57e71436bab6242d7e6abc0
kumapo <kumapo@users.noreply.github.com>
2022-09-30 21:00:51 +0900

    bbox_upper_right should be calculated with the ascent in LTChar

--- pdfminer/layout.py
+++ pdfminer/layout.py
@@ -373,8 +373,9 @@ class LTChar(LTComponent, LTText):
         else:
             # horizontal
             descent = font.get_descent() * fontsize
+            ascent = font.get_ascent() * fontsize
             bbox_lower_left = (0, descent + rise)
-            bbox_upper_right = (self.adv, descent + rise + fontsize)
+            bbox_upper_right = (self.adv, ascent - rise)
         (a, b, c, d, e, f) = self.matrix
         self.upright = 0 < a * d * scaling and b * c <= 0
         (x0, y0) = apply_matrix_pt(self.matrix, bbox_lower_left)
